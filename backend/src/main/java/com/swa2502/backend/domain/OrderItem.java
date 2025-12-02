69695b583ba642377c790e9ce7197a56ccf70a93
seanlancekim0 <122666778+seanlancekim0@users.noreply.github.com>
2025-12-02 14:13:28 +0900

    feat: Item 클래스 삭제, 각 Item 관련 클래스를 상속이 아닌 연관관계로 표현

--- backend/src/main/java/com/swa2502/backend/domain/OrderItem.java
+++ backend/src/main/java/com/swa2502/backend/domain/OrderItem.java
@@ -12,7 +12,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class OrderItem extends Item {
+public class OrderItem {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
